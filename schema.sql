2bc497aca26457c3c58748254dd51f8ffe458dea
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-12 23:49:15 +0000

    add sql to create database and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vetclinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
+
