c14fb4bb87dd3382dce58823676e2c1770dbfc30
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-13 09:27:17 +0000

    update table animals with specified value

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
- an
+
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 SELECT * FROM animals WHERE neutered = true;
--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE DATABASE vetclinic;
+DROP DATABASE IF EXISTS vet_clinic;
+
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -8,9 +10,11 @@ CREATE TABLE animals (
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL,
-    species_id VARCHAR(255) NOT NULL
+    weight_kg DECIMAL NOT NULL
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
 
+/* Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.*/
+BEGIN;
+UPDATE animals SET species = 'unspecified' WHERE species IS NULL;
