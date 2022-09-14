d0ce4081a9fdce33a412052f0bddb79c64fb5a96
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-14 07:12:05 +0000

    alter table with foreign keys

--- schema.sql
+++ schema.sql
@@ -27,3 +27,8 @@ CREATE TABLE species (
     name VARCHAR(255) NOT NULL
 );
 
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
