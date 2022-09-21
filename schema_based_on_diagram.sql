bc1cce53f5b5560e72f3a4d1e1fd745e7360af78
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-21 07:29:01 +0000

    add table invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,3 +15,12 @@ CREATE TABLE medical_histories (
     patient_id BIGINT NOT NULL REFERENCES patients(id),
     status VARCHAR(255) NOT NULL
 );
+
+/* create table invoices */
+CREATE TABLE invoices (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    medical_history_id BIGINT NOT NULL REFERENCES medical_histories(id),
+    total_amount DECIMAL(10,2) NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL
+);
