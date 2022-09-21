590dba8a4ea44abf80f7989e2bd7340c3a9c0e6e
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-21 07:33:38 +0000

    add indexes for performance

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -47,3 +47,10 @@ CREATE TABLE invoice_items (
     invoice_id INT NOT NULL REFERENCES invoices(id),
     treatments_id INT REFERENCES treatments(id)
 );
+
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON treatments_medical_history (treatments_id);
+CREATE INDEX ON treatments_medical_history (medical_histories_id);
+CREATE INDEX ON invoice_items (treatments_id);
