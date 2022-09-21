869836ba0412fdedfe01e4cb2d2c276fb45e2398
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-09-21 07:28:37 +0000

    add table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,11 @@ CREATE TABLE patients (
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL
 );
+
+/* create table medical_histories */
+CREATE TABLE medical_histories (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id BIGINT NOT NULL REFERENCES patients(id),
+    status VARCHAR(255) NOT NULL
+);
